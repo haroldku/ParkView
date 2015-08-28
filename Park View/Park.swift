352aa71327adb60de0d143e0c986e64050954bd2
Harold Kurth <harold.kurth@gmail.com>
2015-08-28 16:58:57 -0500

    Initial comm

---
+++ Park View/Park.swift
@@ -0,0 +1,18 @@
+//
+//  Park.swift
+//  Park View
+//
+//  Created by Harold Kurth on 8/28/15.
+//  Copyright (c) 2015 Chris Wagner. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class Park {
+    var boundary: [CLLocationCoordiante2D]
+    var boundaryPointsCount: NSInteger
+    
+    var midCoordinate: CLLocationCoordinate2D
+    
+}
